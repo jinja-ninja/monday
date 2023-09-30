4f107f11944159e8e9f3e0a05f7a9ba6f82f412c
galbarcessat <galbarcessat1@gmail.com>
2023-09-30 14:03:30 +0300

    first commit test

---
+++ src/cmps/AppHeader.jsx
@@ -0,0 +1,11 @@
+
+import mondayLogo from '../assets/imgs/mondayLogo.png'
+
+export function AppHeader() {
+    return (
+        <div className="header-container main-layout">
+
+            <img src={mondayLogo} alt="" />
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { ProductTab } from '../cmps/ProductTab'
 export function HomePage() {
     return <div className="main-page-layout">
         <main className="main-page-content">
-            {/* <ProductTab /> */}
+
         </main>
     </div>
 }
