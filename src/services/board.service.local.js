6e6c70b634274806345899aecb6917d2bb409aa5
Omer Vered <omervered21@gmail.com>
2023-10-03 23:13:23 +0300

    F/Added from services

---
+++ src/services/board.service.local.js
@@ -0,0 +1,46 @@
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+
+
+
+const STORAGE_KEY = 'board'
+
+export const boardService = {
+    query,
+    get,
+    save,
+    remove,
+    getEmptyBoard
+}
+
+async function query() {
+    return await storageService.query(STORAGE_KEY)
+}
+
+async function get(boardId) {
+    return await storageService.get(STORAGE_KEY, boardId)
+}
+
+async function save(board) {
+    var savedBoard
+    if (board._id) {
+        savedBoard = await storageService.put(STORAGE_KEY, board)
+    } else {
+        savedBoard = await storageService.post(STORAGE_KEY, board)
+    }
+    return savedBoard
+}
+
+async function remove(boardId) {
+    return await storageService.remove(STORAGE_KEY, boardId)
+}
+
+function getEmptyBoard() {
+    return {
+        title: '',
+        createdAt: Date.now(),
+        createdBy: {
+            _id: 'u101'
+        }
+    }
+}
