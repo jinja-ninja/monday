cb03fd33ef31a10afa283e90f92ee5ff29cea998
Omer Vered <omervered21@gmail.com>
2023-10-03 18:32:43 +0300

    F/added page not found and redirect to home page

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import { useHistory } from 'react-router-dom'
+
+export function PageNotFound() {
+
+    const history = useHistory()
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            history.push('/');
+        }, 3000); // Redirect after 3 seconds (adjust the delay as needed)
+    }, [history]);
+
+    return (
+        <div>
+            <h1>PageNotFound</h1>
+            <nav>   </nav>
+        </div>
+    )
+}
