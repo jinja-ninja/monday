12cb978fa1c444076ddd219295bb78af410b1e67
galbarcessat <galbarcessat1@gmail.com>
2023-10-04 13:01:53 +0300

    rendering boards to side bar starter

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,15 +1,144 @@
 import { useState } from "react";
 import { Button, IconButton, MenuButton, MenuItem, SearchComponent, Menu } from "monday-ui-react-core";
 import { Add, Board, Delete, Home, Edit } from "monday-ui-react-core/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { BoardNavLink } from "./BoardNavLink";
 
-
+const boards = [{
+    _id: "b101",
+    title: "Robot dev proj",
+    isStarred: false,
+    archivedAt: 1589983468418,
+    createdBy: {
+      _id: "u101",
+      fullname: "Abi Abambi",
+      imgUrl: "http://some-img"
+    },
+    style: {
+      backgroundImage: ""
+    },
+    labels: [
+      {
+        id: "l101",
+        title: "Done",
+        color: "#61BD4F"
+      },
+      {
+        id: "l102",
+        title: "Progress",
+        color: "#61BD33"
+      }
+    ],
+    members: [
+      {
+        _id: "u101",
+        fullname: "Tal Tarablus",
+        imgUrl: "https://www.google.com"
+      }
+    ],
+    groups: [
+      {
+        id: "g101",
+        title: "Group 1",
+        archivedAt: 1589983468418,
+        tasks: [
+          {
+            id: "c101",
+            title: "Replace logo"
+          },
+          {
+            id: "c102",
+            title: "Add Samples"
+          }
+        ],
+        style: {}
+      },
+      {
+        id: "g102",
+        title: "Group 2",
+        tasks: [
+          {
+            id: "c103",
+            title: "Do that",
+            archivedAt: 1589983468418
+          },
+          {
+            id: "c104",
+            title: "Help me",
+            status: "in-progress",
+            priority: "high",
+            description: "description",
+            comments: [
+              {
+                id: "ZdPnm",
+                txt: "also @yaronb please CR this",
+                createdAt: 1590999817436,
+                byMember: {
+                  _id: "u101",
+                  fullname: "Tal Tarablus",
+                  imgUrl: "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                }
+              }
+            ],
+            checklists: [
+              {
+                id: "YEhmF",
+                title: "Checklist",
+                todos: [
+                  {
+                    id: "212jX",
+                    title: "To Do 1",
+                    isDone: false
+                  }
+                ]
+              }
+            ],
+            memberIds: ["u101"],
+            labelIds: ["l101", "l102"],
+            dueDate: 16156215211,
+            byMember: {
+              _id: "u101",
+              username: "Tal",
+              fullname: "Tal Tarablus",
+              imgUrl: "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+            },
+            style: {
+              backgroundColor: "#26DE81"
+            }
+          }
+        ],
+        style: {}
+      }
+    ],
+    activities: [
+      {
+        id: "a101",
+        txt: "Changed Color",
+        createdAt: 154514,
+        byMember: {
+          _id: "u101",
+          fullname: "Abi Abambi",
+          imgUrl: "http://some-img"
+        },
+        group: {
+          id: "g101",
+          title: "Urgent Stuff"
+        },
+        task: {
+          id: "c101",
+          title: "Replace Logo"
+        }
+      }
+    ],
+    cmpsOrder: ["StatusPicker", "MemberPicker", "DatePicker"]
+  }]
 
 export function SideBar() {
     const [isOpen, setIsOpen] = useState(false)
+    // const dispatch = useDispatch()
+    // const boards = useSelector(storeState => storeState.boardModule.boards)
 
-    function openMenu() {
-        console.log('menu clicked:')
-    }
+
 
     function showBoard() {
         console.log('board clicked:')
@@ -77,17 +206,29 @@ export function SideBar() {
                     />
 
                 </div>
+
+
+                {/* render BoardNavLink */}
                 <div className="boards-btns-container">
-                   
+
+                    {/* {boards && boards.map((board, idx) => {
+                        return (
+                            <BoardNavLink text={board.title} key={idx} />
+                        )
+                    })} */}
+
                     <Button
                         className="btn-board"
                         kind="tertiary"
                         leftIcon={Board}
-                    // onClick={function noRefCheck() { }}
+                        onClick={() => showBoard()
+
+                        }
                     >
                         Monday Funday
                         <MenuButton closeDialogOnContentClick className="btn-board-menu" size={MenuButton.sizes.XS} onClick={(e) => {
                             e.stopPropagation()
+                            console.log('MENU CLICKED:')
                         }}>
                             <Menu id="menu" size={Menu.sizes.MEDIUM}>
                                 {/* <MenuTitle caption="Look up, you might see" captionPosition={MenuTitle.positions.TOP} /> */}
@@ -101,7 +242,7 @@ export function SideBar() {
                         className="btn-board"
                         kind="tertiary"
                         leftIcon={Board}
-                    // onClick={function noRefCheck() { }}
+                        onClick={() => showBoard()}
                     >
                         Gal Surf Trip
 
@@ -119,6 +260,6 @@ export function SideBar() {
 
             </div>
 
-        </div>
+        </div >
     )
 }
