9f94da6a9859e11715d86468b592a99ab84743dd
galbarcessat <galbarcessat1@gmail.com>
2023-10-31 20:18:59 +0200

    V5 fuck scroll

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -8,45 +8,49 @@ export function GroupList({ groups, labels, cmpsOrder, boardId, priorities, hidd
 
     let filteredCmpOrder = cmpsOrder.filter(cmp => !hiddenColumns.map(str => str.toLowerCase()).includes(cmp.toLowerCase()))
 
-    return <div className="group-list-container">
 
-        <div className="side-spacing-div"></div>
+    return (
+        <section className="groups-section">
+            <div className="side-spacing-div"></div>
+            <div className="group-list-container">
 
-        <Droppable droppableId="groups" type="groups">
-            {(provided, snapshot) => (
-                <ul
-                    className="group-list"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}>
-                    {groups && groups.map((group, index) => {
-                        return <li key={group.id} className="group-list-item" data-group-id={group.id}>
-                            <GroupPreview
-                                index={index}
-                                group={group}
-                                boardId={boardId}
-                                labels={labels}
-                                cmpsOrder={filteredCmpOrder}
-                                priorities={priorities}
-                                collapseAll={snapshot.isDraggingOver}
-                                hiddenColumns={hiddenColumns}
-                                key={`group-preview-${group.id}`} />
-                        </li>
-                    })}
-                    {provided.placeholder}
-                </ul >
-            )}
-        </Droppable>
+                <Droppable droppableId="groups" type="groups">
+                    {(provided, snapshot) => (
+                        <ul
+                            className="group-list"
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}>
+                            {groups && groups.map((group, index) => {
+                                return <li key={group.id} className="group-list-item" data-group-id={group.id}>
+                                    <GroupPreview
+                                        index={index}
+                                        group={group}
+                                        boardId={boardId}
+                                        labels={labels}
+                                        cmpsOrder={filteredCmpOrder}
+                                        priorities={priorities}
+                                        collapseAll={snapshot.isDraggingOver}
+                                        hiddenColumns={hiddenColumns}
+                                        key={`group-preview-${group.id}`} />
+                                </li>
+                            })}
+                            {provided.placeholder}
+                        </ul >
+                    )}
+                </Droppable>
 
-        {groups.length !== 0 && <Button
-            onClick={() => { addGroup(boardId) }}
-            kind="secondary"
-            size="small"
-            className="btn-add-group"
-            leftIcon={Add}>
-            Add new group
-        </Button>}
+                {groups.length !== 0 && <Button
+                    onClick={() => { addGroup(boardId) }}
+                    kind="secondary"
+                    size="small"
+                    className="btn-add-group"
+                    leftIcon={Add}>
+                    Add new group
+                </Button>}
 
 
-    </div >
+            </div >
+        </section>
+    )
 }
 
