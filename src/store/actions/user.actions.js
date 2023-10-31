3e62e177d5356287aef35845ff3044331cf20538
Nati <7natifel@gmail.com>
2023-11-01 00:56:34 +0200

    F/Users

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -26,11 +26,7 @@ export async function removeUser(userId) {
 export async function login(credentials) {
     try {
         const user = await userService.login(credentials)
-        store.dispatch({
-            type: SET_USER,
-            user
-        })
-        socketService.login(user)
+        store.dispatch({ type: SET_USER, user })
         return user
     } catch (err) {
         console.log('Cannot login', err)
@@ -41,11 +37,7 @@ export async function login(credentials) {
 export async function signup(credentials) {
     try {
         const user = await userService.signup(credentials)
-        store.dispatch({
-            type: SET_USER,
-            user
-        })
-        socketService.login(user)
+        store.dispatch({ type: SET_USER, user })
         return user
     } catch (err) {
         console.log('Cannot signup', err)
@@ -56,11 +48,7 @@ export async function signup(credentials) {
 export async function logout() {
     try {
         await userService.logout()
-        store.dispatch({
-            type: SET_USER,
-            user: null
-        })
-        socketService.logout()
+        store.dispatch({ type: SET_USER, user: null })
     } catch (err) {
         console.log('Cannot logout', err)
         throw err
