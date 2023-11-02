f90341f015990eac3731f4c9517227ab72b69ee2
Omer Vered <omervered21@gmail.com>
2023-11-02 16:04:00 +0200

    added succesfully

--- src/cmps/Dashboard/DashboardDetails.jsx
+++ src/cmps/Dashboard/DashboardDetails.jsx
@@ -9,165 +9,206 @@ export function DashboardDetails() {
     const [size, setSize] = useState({ width: 350, height: 350 })
     const chartRef = useRef(null);
 
+
     HighchartsMore(Highcharts);
     HighchartsExporting(Highcharts);
 
-    const options = {
+
+    const totalTasks = [35, 45, 67, 70, 65, 74, 89, 88, 79, 68, 72, 61]
+    const doneTasks = [23, 40, 67, 51, 64, 70, 36, 69, 72, 43, 60, 45]
+
+    const sumOfTotalTasks = totalTasks.reduce((acc, currVal) => acc + currVal, 0)
+    const sumOfDoneTasks = doneTasks.reduce((acc, currVal) => acc + currVal, 0)
+
+    const percentage = ((sumOfDoneTasks / sumOfTotalTasks) * 100).toFixed(2)
+    console.log('percentage:', percentage)
+
+    const tasksOptions = {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'R&D tasks completed ontime'
+        },
+        xAxis: {
+            categories: [, 'Nov 22', 'Dec 22', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct'],
+            accessibility: {
+                description: 'Months of the year'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Tasks'
+            },
+
+        },
+        series: [{
+            name: 'Total Tasks',
+            data: totalTasks,
+            color: 'var(--color-done-green)'
+        }, {
+            name: 'Finished Before DueDate',
+            data: doneTasks
+        }]
+    }
+
+    const totalTasksPercentageOptions = {
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: '% Of Tasks Completed On Time'
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        plotOptions: {
+            series: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: [{
+                    enabled: true,
+                    distance: 20
+                }, {
+                    enabled: true,
+                    distance: -40,
+                    format: '{point.percentage:.1f}%',
+                    style: {
+                        fontSize: '1.4em',
+                        textOutline: 'none',
+                        opacity: 0.7
+                    },
+                    filter: {
+                        operator: '>',
+                        property: 'percentage',
+                        value: 10
+                    }
+                }]
+            }
+        },
+        series: [
+            {
+                name: 'Percentage',
+                data: [
+                    {
+                        name: 'Done On Time',
+                        y: parseFloat(percentage)
+                    },
+                    {
+                        name: 'Not Completed On Time',
+                        y: parseFloat((100 - percentage).toFixed(1))
+                    }
+                ]
+            }
+        ]
+    }
+
+    const optionsOmer = {
         chart: {
             type: 'column'
         },
         title: {
-            text: 'Total Tasks status'
+            text: 'Omer V tasks status'
         },
         xAxis: {
             categories: ['Done', 'In Progress', 'Stuck']
         },
         series: [{
             data: [
-                { y: 22, color: `var(--color-done-green)` },
-                { y: 12, color: `var(--color-working_orange)` },
-                { y: 4, color: `var(--color-stuck-red)` }
+                { y: 22, color: 'var(--color-done-green)' },
+                { y: 12, color: 'var(--color-working_orange)' },
+                { y: 4, color: 'var(--color-stuck-red)' }
             ]
         }]
     }
-    const options1 = {
-
-        title: {
-            text: 'Task Per Member'
-        },
-        xAxis: {
-            categories: ['Omer Vered', 'Nati Feldbaum', 'Gal Ben Natan']
-        },
-        series: [{
-            data: [
-                { y: 45 },
-                { y: 11 },
-                { y: 28 }
-            ]
-        }]
-    }
-    const options2 = {
-
-        title: {
-            text: 'Total Members on Board (Past 5 Months)'
-        },
-        xAxis: {
-            categories: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov']
-        },
-        series: [{
-            data: [
-                { y: 2 },
-                { y: 3 },
-                { y: 6 },
-                { y: 4 },
-                { y: 5 }
-            ]
-        }]
-    }
-
-    const pieOptions = {
+    const optionsNati = {
         chart: {
-            type: 'pie',
-            plotShadow: false,
-            backgroundColor: 'transparent'
+            type: 'column'
         },
         title: {
-            text: 'Active Users on Total Boards'
-        },
-        plotOptions: {
-            pie: {
-                innerSize: '50%',
-                depth: 45
-            }
-        },
-        series: [{
-            name: 'Data',
-            data: [
-                ['Management', 10],
-                ['Team Leaders', 60],
-                ['Group Leaders', 30]
-            ]
-        }]
-    };
-
-
-    const chart1Options = {
-        chart: {
-            type: 'column',
-            backgroundColor: '#f5f5f5',
-            borderRadius: 5
-        },
-        title: {
-            text: 'Sales Data',
-            style: {
-                color: '#333333',
-                fontWeight: 'bold',
-                fontSize: '18px'
-            }
+            text: 'Nati F tasks status'
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-            labels: {
-                style: {
-                    color: '#666666'
-                }
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Amount',
-                style: {
-                    color: '#666666'
-                }
-            },
-            labels: {
-                style: {
-                    color: '#666666'
-                }
-            }
+            categories: ['Done', 'In Progress', 'Stuck']
         },
         series: [{
-            name: 'Sales',
-            data: [100, 200, 150, 300, 250, 180],
-            color: '#7cb5ec'
-        }],
-        credits: {
-            enabled: false
-        }
+            data: [
+                { y: 12, color: 'var(--color-done-green)' },
+                { y: 10, color: 'var(--color-working_orange)' },
+                { y: 18, color: 'var(--color-stuck-red)' }
+            ]
+        }]
     }
+    const optionsGal = {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Gal B tasks status'
+        },
+        xAxis: {
+            categories: ['Done', 'In Progress', 'Stuck']
+        },
+        series: [{
+            data: [
+                { y: 17, color: 'var(--color-done-green)' },
+                { y: 5, color: 'var(--color-working_orange)' },
+                { y: 3, color: 'var(--color-stuck-red)' }
+            ]
+        }]
+    }
+
 
     return (
         // <div className="dashboard-container">
         //     <img className='in-progress-img' src={WorkInProgress} alt="" />
         // </div>
         <div className='dashboard-container'>
-
-            <div className='box show-border' style={{ width: size.width + 'px', height: size.height + 'px' }} >
+            <div className='chart-data-container'>
+                <div className='chart-info show-border'>
+                    <div className='completed-tasks'>
+                        <h1>Completed tasks ontime</h1>
+                        <p>78.72 %</p>
+                    </div>
+                </div>
+                <div className='chart-info show-border'>
+                    <div className='completed-tasks'>
+                        <h1>Performance months to review</h1>
+                        <p style={{ color: 'var(--color-stuck-red)' }}> Jan, Apr, Jul</p>
+                    </div>
+                </div>
+            </div>
+            <div className='line-chart'>
                 <HighchartsReact
                     highcharts={Highcharts}
-                    options={options}
-                    ref={chartRef}
+                    options={tasksOptions}
                 />
             </div>
-            <div className='box show-border' style={{ width: size.width + 'px', height: size.height + 'px' }} >
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={options1}
-                    ref={chartRef}
-                />
-            </div>
-            <div className='box show-border' style={{ width: size.width + 'px', height: size.height + 'px' }} >
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={options2}
-                    ref={chartRef}
-                />
-            </div>
+            <div className='members-chart'>
+                <div className='box show-border'>
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={optionsOmer}
+                        ref={chartRef}
+                    />
+                </div>
+                <div className='box show-border'>
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={optionsNati}
+                        ref={chartRef}
+                    />
+                </div>
+                <div className='box show-border'>
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={optionsGal}
+                        ref={chartRef}
+                    />
+                </div>
 
-            <div className='box1 show-border'>
-                <HighchartsReact highcharts={Highcharts} options={pieOptions} />
-            </div>
-
+            </div >
         </div >
     )
 }
